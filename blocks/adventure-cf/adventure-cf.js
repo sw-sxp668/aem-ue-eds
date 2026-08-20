532d5d63a1f67b67afabfdad3aa40cbc61ba2d8f
adls <student@example.com>
2026-08-20 21:35:02 +0000

    initial commit content fragment graphql js update new

--- blocks/adventure-cf/adventure-cf.js
+++ blocks/adventure-cf/adventure-cf.js
@@ -4,7 +4,7 @@
  */
 
 // eslint-disable-next-line import/no-unresolved
-import { getAdventureByPath } from '../../scripts/aem-gql-connection.js';
+import { getAEMHost, getAdventureByPath } from '../../scripts/aem-gql-connection.js';
 
 /**
  * Show error state
@@ -28,6 +28,77 @@ function formatLabel(key) {
   return key.charAt(0).toUpperCase() + key.slice(1).replace(/([A-Z])/g, ' $1');
 }
 
+function createDisplay(contentFragment) {
+  const { keys } = contentFragment; // String version of keys in adventureByPath query
+  const { data } = contentFragment;
+
+  let innerHTML = '';
+  // eslint-disable-next-line no-underscore-dangle
+  const cfPath = data._path;
+
+  // data-aue-resource
+  //    Connects to the source content fragment for editing capabilities in the Universal Editor
+  // data-aue-type
+  //    Tells the Universal Editor this is a reference item
+  // data-aue-label
+  //    Title of the phantom reference item under the block in the Universal Editor Content Tree
+  innerHTML
+  += `<div class="headless-wrapper">
+    <div class="content-fragment-detail"
+        data-aue-resource="urn:aemconnection:${cfPath}/jcr:content/data/master" 
+        data-aue-type="reference" 
+        data-aue-label="${data[keys.title]}">`;
+  // eslint-disable-next-line no-underscore-dangle
+  const cfPrimaryImagePath = data[keys.primaryImage]._path;
+  // Any element that contains a CF data property that
+  //    should be editable in the Universal Editor needs to have:
+  // data-aue-type
+  //    Tells the Universal Editor what type of form field the data is
+  // data-aue-prop
+  //    Tells the Universal Editor what property to save back to the content fragment
+  innerHTML
+        += `<div class="content-fragment-hero">
+            <div class="content-fragment-image">
+                <picture>
+                    <source srcset="${getAEMHost()}${cfPrimaryImagePath}?width=1200&format=webply&optimize=medium" type="image/webp">
+                    <img src="${getAEMHost()}${cfPrimaryImagePath}?width=1200&format=webply&optimize=medium" alt="${data[keys.title]}" loading="lazy" data-aue-type="media" data-aue-prop="${keys.primaryImage}">
+                </picture>
+            </div>
+            <div class="content-fragment-${keys.title}-overlay">
+                <h1 class="content-fragment-${keys.title}" data-aue-type="text" data-aue-prop="${keys.title} ">${data[keys.title]}</h1>
+            </div>
+        </div>
+        <div class="content-fragment-content">
+            <div class="content-fragment-details-grid">`;
+  const details = [keys.activity, keys.difficulty, keys.tripLength, keys.groupSize, keys.price];
+  details.forEach((detail) => {
+    if (data[detail]) {
+      innerHTML += `<div class="content-fragment-detail-item">
+                        <span class="detail-label">${formatLabel(detail)}</span>
+                        <span class="detail-value" data-aue-type="text" data-aue-prop="${detail}">${data[detail]}</span>
+                    </div>`;
+    }
+  });
+  innerHTML
+        += `</div>
+            <div class="content-fragment-${keys.description}">
+                  <h2>About This Adventure</h2>
+                  <div class="content-fragment-${keys.description}-content" data-aue-type="richtext" data-aue-prop="${keys.description}">
+                      ${data[keys.description].html || data[keys.description].plaintext}
+                  </div>
+              </div>
+              <div class="content-fragment-${keys.itinerary}">
+                  <h2>Itinerary</h2>
+                  <div class="content-fragment-${keys.itinerary}-content" data-aue-type="richtext" data-aue-prop="${keys.itinerary}">
+                      ${data[keys.itinerary].html || data[keys.itinerary].plaintext}
+                  </div>
+              </div>
+          </div>
+    </div>
+    </div>`;
+  return innerHTML;
+}
+
 /**
  * Main decoration function
  */
@@ -49,19 +120,15 @@ export default async function decorate(block) {
       return;
     }
 
-    // eslint-disable-next-line no-console
-    console.log('Content Fragment:', contentFragment.data); // Retrieves data from the content fragment based on the adventure-by-path query
-    // eslint-disable-next-line no-console
-    console.log('Content Fragment:', contentFragment.keys); // Retrieves keys defined in the adventure-by-path query
-
-    let display = '';
-    Object.keys(contentFragment.data).forEach((key) => {
-      const label = formatLabel(key);
-      const value = contentFragment.data[key];
-      display += `<div> ${label}: ${value} </div>`;
-    });
-
-    block.innerHTML = display;
+    /*
+    Because the content fragment is a reference property,
+      we can rewrite the entire block with the content fragment data
+    Caution with doing this with default content and inferred elements
+      since UE it renders the block with special aue attributes
+    Learn more about inferred elements here:
+      https://www.aem.live/developer/component-model-definitions#creating-semantic-content-models-for-blocks
+    */
+    block.innerHTML = createDisplay(contentFragment);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Content Fragment block error:', error);
